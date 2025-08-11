3682347c0378ee8e43af222b091d3b08c411701b
Joseline Del Angel <yosska000@gmail.com>
2025-08-11 17:26:30 -0600

    implementar funcion para agregar amigos

--- app.js
+++ app.js
@@ -2,3 +2,21 @@
 
 //declarando una variable de tipo array
 let amigos=[];
+
+function agregarAmigo(){
+   
+    //Capturar el valor del campo de entrada
+    let nuevoNombre =document.getElementById('amigo').value;
+
+    //Validar la entrada
+    if (nuevoNombre == ''){
+        alert("Por favor inserte un nombre");
+    }else{
+        //Actualizar el array de amigos
+        nombres.push(nuevoNombre);
+
+        //Limpiar el campo de entrada
+        document.getElementById('amigo').value ='';
+                
+    }     
+}
